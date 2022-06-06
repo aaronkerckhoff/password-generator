57314c355512c9ed81230f57ae761f3bb27ca952
Aaron Kerckhoff <aaronkerckhoff@gmail.com>
2022-06-06 14:17:38 +0200

    Add check if password includes at least one letter, one number and one special character

--- main.py
+++ main.py
@@ -2,7 +2,10 @@ import secrets
 import pyperclip as pyperclip
 
 # Get all available characters
-characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()!@#$%^&*()!@#$%^&*()!@#$%^&*()'
+characters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()'
+letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+numbers = '0123456789'
+special_characters = '!@#$%^&*()'
 
 
 def generate_password():
@@ -14,7 +17,27 @@ def generate_password():
     return password
 
 
-# Copy password to clipboard
-generated_password = generate_password()
+generated_password = ''
+
+
+# Check if password includes at least one letter, one number, and one special character
+# If not, generate a new password
+
+
+def contains_letter(password):
+    return any(c in letters for c in password)
+
+
+def contains_number(password):
+    return any(c in numbers for c in password)
+
+
+def contains_special_character(password):
+    return any(c in special_characters for c in password)
+
+
+while not contains_letter(generated_password) or not contains_number(generated_password) or not contains_special_character(generated_password):
+    generated_password = generate_password()
+
 pyperclip.copy(generated_password)
 print(generated_password)
