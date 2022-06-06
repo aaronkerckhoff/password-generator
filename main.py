926c989da5a26578cca56be509492817cdfe1c2e
Aaron Kerckhoff <aaronkerckhoff@gmail.com>
2022-06-06 14:29:59 +0200

    Differentiate between lowercase and uppercase letters

--- main.py
+++ main.py
@@ -2,7 +2,8 @@ import secrets
 import pyperclip as pyperclip
 
 # Get all available characters
-letters = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'
+lowercase_letters = 'abcdefghijklmnopqrstuvwxyz'
+uppercase_letters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 numbers = '0123456789'
 special_characters = '!@#$%^&*()'
 
@@ -11,7 +12,7 @@ def generate_password():
     password = ''
     for i in range(15):
         # Generate a random character
-        character = secrets.choice(letters + numbers + special_characters)
+        character = secrets.choice(lowercase_letters + uppercase_letters + numbers + special_characters)
         password += character
     return password
 
@@ -22,8 +23,12 @@ generated_password = ''
 # Check if password includes at least one letter, one number, and one special character
 # If not, generate a new password
 
-def contains_letter(password):
-    return any(c in letters for c in password)
+def contains_lowercase_letter(password):
+    return any(c in lowercase_letters for c in password)
+
+
+def contains_uppercase_letter(password):
+    return any(c in uppercase_letters for c in password)
 
 
 def contains_number(password):
@@ -34,7 +39,7 @@ def contains_special_character(password):
     return any(c in special_characters for c in password)
 
 
-while not contains_letter(generated_password) or not contains_number(generated_password) or not contains_special_character(generated_password):
+while not contains_lowercase_letter(generated_password) or not contains_uppercase_letter(generated_password) or not contains_number(generated_password) or not contains_special_character(generated_password):
     generated_password = generate_password()
 
 pyperclip.copy(generated_password)
